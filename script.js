b37375048f1aecfb2a0b818917038c2177392cdc
Lim Kopi Singapore Community <limkopisingapore@gmail.com>
2020-06-21 19:34:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,4 +27,18 @@ window.smoothScroll = function (target) {
   scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 };
 
+var acc = document.getElementsByClassName("accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+
 
